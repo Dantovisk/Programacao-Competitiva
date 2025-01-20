13d7e10083e78c5c4bd4578d9e681f9171ea9a4a
Dante Brito <danbrilou@gmail.com>
2025-01-20 18:27:17 -0300

    novo exercicio

--- GEMA/Grafos/LCA/Company_queries_RMQ.cpp
+++ GEMA/Grafos/LCA/Company_queries_RMQ.cpp
@@ -1,19 +1,32 @@
-#include <bits/stdc++.h> // resolveremos com LCA por Binary Lifting
+#include <bits/stdc++.h> // resolveremos com euler tour e sparse table
 #define MAXN 200010
 
 using namespace std;
 
 vector <int> adj[MAXN];
-int ances [MAXN][20];
 int level[MAXN];
+int tin[MAXN];
+
+int vec[2*MAXN];
+int table [2*MAXN][20];   // o elemento que possui menor nivel
+int temp = 0;
+
+//o problema se resumirá a RMQ no array
+//Poderíamos resolver com segtree, mas sparse table
+//É mais eficiente e simples nesse caso (não há alterações)
 
 void dfs(int u, int p){
-    ances[u][0] = p;
+    vec[temp] = u;
+    tin[u] = temp;
+    temp++;
 
     for(auto v: adj[u]){
         if(v != p){
             level[v] = level[u] + 1;
             dfs(v, u);
+
+            vec[temp] = u;
+            temp++;
         }
     }
 }
@@ -34,16 +47,38 @@ int main(){
         adj[a].push_back(i);
     }
 
-    memset(ances, -1, sizeof(ances));
+    memset(tin, -1, sizeof(tin));
 
     level[1] = 0;
-
     dfs(1, -1);
+
+    for(int i=0; i<temp; i++){
+        table[i][0] = vec[i];
+    }
+
+    for(int k=1; k<20; k++){
+        for(int i=0; i + (1<<k) <= temp; i++){
+            int l = table[i][k-1];
+            int r = table[i + (1<<(k-1))][k-1];
+            table[i][k] = (level[l]<level[r] ? l : r);
+        }
+    }
 
     for(int i=0; i<q; i++){
         int a, b;
         cin>>a>>b;
 
+        a=tin[a];
+        b=tin[b];
+
+        if(a>b) swap(a,b);
+
+        int k = log2(b-a+1);
+
+        int l = table[a][k];
+        int r = table[(b-(1<<k))+1][k];
+
+        cout<<(level[l] < level [r]? l : r)<<"\n";
     }
 
     return 0;
