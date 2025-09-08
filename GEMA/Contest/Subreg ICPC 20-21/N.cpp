ab6ff5ca4de178564ddc9269347b731d26ee840c
Dante Brito <danbrilou@gmail.com>
2025-09-08 10:30:34 -0300

    Fast factorization

--- GEMA/Contest/Subreg ICPC 20-21/N.cpp
+++ GEMA/Contest/Subreg ICPC 20-21/N.cpp
@@ -14,16 +14,48 @@ typedef vector<int> vi;
 const int MAX = 1e3+ 15;
 const ll mod  = 1e9+7;
 
-const int MAX_PR = 33'000'005;
-bitset<MAX_PR> isprime;
-vi eratosthenesSieve(int lim=33'000'002) {
-	isprime.set(); isprime[0] = isprime[1] = 0;
-	for (int i = 4; i < lim; i += 2) isprime[i] = 0;
-	for (int i = 3; i*i < lim; i += 2) if (isprime[i])
-		for (int j = i*i; j < lim; j += i*2) isprime[j] = 0;
-	vi pr;
-	rep(i,2,lim) if (isprime[i]) pr.push_back(i);
-	return pr;
+typedef unsigned long long ull;
+ull modmul(ull a, ull b, ull M) {
+	ll ret = a * b - M * ull(1.L / M * a * b);
+	return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod) {
+	ull ans = 1;
+	for (; e; b = modmul(b, b, mod), e /= 2)
+		if (e & 1) ans = modmul(ans, b, mod);
+	return ans;
+}
+
+bool isPrime(ull n) {
+	if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+	ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+	    s = __builtin_ctzll(n-1), d = n >> s;
+	for (ull a : A) {   // ^ count trailing zeroes
+		ull p = modpow(a%n, d, n), i = s;
+		while (p != 1 && p != n - 1 && a % n && i--)
+			p = modmul(p, p, n);
+		if (p != n-1 && i != s) return 0;
+	}
+	return 1;
+}
+
+ull pollard(ull n) {
+	ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+	auto f = [&](ull x) { return modmul(x, x, n) + i; };
+	while (t++ % 40 || __gcd(prd, n) == 1) {
+		if (x == y) x = ++i, y = f(x);
+		if ((q = modmul(prd, max(x,y) - min(x,y), n))) prd = q;
+		x = f(x), y = f(f(y));
+	}
+	return __gcd(prd, n);
+}
+vector<ull> factor(ull n) {
+	if (n == 1) return {};
+	if (isPrime(n)) return {n};
+	ull x = pollard(n);
+	auto l = factor(x), r = factor(n / x);
+	l.insert(l.end(), all(r));
+	return l;
 }
 
 set <ll> factors;
@@ -36,20 +68,11 @@ int main(){
     int m, n, d;
     cin>>m>>n>>d;
 
-    vi primo = eratosthenesSieve();
-
     for(int i =0; i<n; i++){
         cin>>a[i];
-        ll u = a[i];
-        for(auto p: primo){
-            if(u%p == 0) factors.insert(p);
-            while(u%p == 0){
-                u/= p;
-            }   
-
-            if(u == 1) break;
-        }
-        if(u > 1) factors.insert(u);
+        
+        vector<ull> facs = factor(a[i]);
+        for(auto f: facs) factors.insert(f);
     }
 
     for(int i =0; i<d; i++){
