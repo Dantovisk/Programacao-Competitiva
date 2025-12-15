cc47731af584e25210e01ee2c54a4713ce1496a9
Dante Brito <danbrilou@gmail.com>
2025-12-15 14:07:56 -0300

    probleminha legal

--- GEMA/Contest/CF 1065/G.cpp
+++ GEMA/Contest/CF 1065/G.cpp
@@ -9,53 +9,90 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 #define ff first
 #define ss second
+#define pb push_back
 
+const ll mod = 1e6+3;
 
-struct UF {
-	vi e;
-	UF(int n) : e(n, -1) {}
-	bool sameSet(int a, int b) { return find(a) == find(b); }
-	int size(int x) { return -e[find(x)]; }
-	int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
-	bool join(int a, int b) {
-		a = find(a), b = find(b);
-		if (a == b) return false;
-		if (e[a] > e[b]) swap(a, b);
-		e[a] += e[b]; e[b] = a;
-		return true;
-	}
-};
+ll fac[mod+3];
+
+ll add(ll a, ll b){
+    return (a + b)%mod;
+}
+
+ll mul(ll a, ll b){
+    return (a * b)%mod;
+}
+
+ll modpow(ll b, ll e) {
+	ll ans = 1;
+	for (; e; b = b * b % mod, e /= 2)
+		if (e & 1) ans = ans * b % mod;
+	return ans;
+}
+
+ll inv(ll a){
+    return modpow(a, mod-2);
+}
 
 void solve(){
     int n; cin>>n;
-    vi vis(n+1, 0);   
-    vi primes = {2, 3, 5, 7, 11, 13, 17};
-    vi res;
+    vi v1(n), v2(n);
 
-    for(auto p: primes){
-        for(int i = p; i <=n; i+= p){
-            if(vis[i]) continue;
+    rep(i, 0 , n) cin>>v1[i];
+    rep(i, 0 , n) cin>>v2[i];
 
-            vis[i] = 1;
-            res.push_back(i);
+    int mindb = 30;
+    rep(i, 0, n){
+        int a = v1[i];
+        int ct = 0;
+        while((a<<1) <= v2[i]){
+            a<<=1;
+            ct++;
         }
+        mindb = min(mindb, ct);
     }
+    vector<vi> adds (n, vi(23, 0));
     
-    vi falt;
-    rep(i, 1, n+1) if(!vis[i]) falt.push_back(i);
+    ll res = mindb;
+    rep(i, 0, n){
+        int a = (v1[i]<<mindb);
+        for(int s = mindb; s >= 0; s--){
+            int diff = v2[i] - a;
+            int curr = (1<<s);
+            
+            adds[i][s] += diff/curr;
+            diff %= curr;
 
-    int j = 0;
-    for(int i = 0; i<res.size(); i++){
-        cout<<res[i]<<" ";
-        if(i <res.size()-1){
-            cout<<res[++i]<<" ";
-        }
-
-        if(j < falt.size()){
-            cout<<falt[j++]<<" ";
+            res += adds[i][s];
+            a = v2[i] - diff;
         }
     }
-    cout<<"\n";
+
+    cout<<res<<" ";
+
+    ll ct = 1;
+
+    for(int s = mindb; s >= 0; s--){
+        ll total = 1;
+        ll curr = 0;
+        rep(i, 0, n){
+            curr += adds[i][s];
+
+            if(adds[i][s] > 1)
+                total = mul(total, inv(fac[adds[i][s]]));
+        }
+        if(curr < mod){
+            total = mul(fac[curr], total);
+            ct = mul(total, ct);
+        }
+        else{
+            ct = 0;
+            break;
+        }
+    }
+
+    cout<<ct<<"\n";
+    
 }
 
 int main() {
@@ -63,6 +100,11 @@ int main() {
 	cin.exceptions(cin.failbit);
 
     int t = 1; cin>>t;
+
+    fac[0] = 1;
+    rep(i, 1, mod+2){
+        fac[i] = mul(fac[i-1], i);
+    }
 
     while(t--)solve();
 }
